39f26bed7e5bde146bd05aadacbe33e2d2432386
shawa <shawa@localhost>
2025-07-02 23:21:45 +0200

    fix: Compilation Error, DMG building
    I think this is the final fix!
    
    There was an incompatible return type error. I presume this is
    because since this was last edited, the Obj-C compiler has gotten
    more strict.
    
    Secondly, it was failing to build the DMG, becuase of permissioning
    errors. Turning off sandboxing fixed it. Not a great option but sure
    look, it builds.

--- Genius1/GeniusAssociationEnumerator.m
+++ Genius1/GeniusAssociationEnumerator.m
@@ -1,19 +1,19 @@
 /*
-	Genius
-	Copyright (C) 2003-2006 John R Chang
-	Copyright (C) 2007-2008 Chris Miner
+    Genius
+    Copyright (C) 2003-2006 John R Chang
+    Copyright (C) 2007-2008 Chris Miner
 
-	This program is free software; you can redistribute it and/or
-	modify it under the terms of the GNU General Public License
-	as published by the Free Software Foundation; either version 2
-	of the License, or (at your option) any later version.
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.	
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.    
 
-	http://www.gnu.org/licenses/gpl.txt
+    http://www.gnu.org/licenses/gpl.txt
 */
 
 #import "GeniusAssociationEnumerator.h"
@@ -34,7 +34,7 @@ static float PoissonValue(int x, float m)
 
 
 //! randomly returns NSOrderedAscending or NSOrderedDescending
-int RandomSortFunction(id object1, id object2, void * context)
+NSComparisonResult RandomSortFunction(id object1, id object2, void * context)
 {
     BOOL x = random() & 0x1;
     return (x ? NSOrderedAscending : NSOrderedDescending);
